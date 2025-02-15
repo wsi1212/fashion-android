d5b310470b53e03943b78a365486be48cb9c3725
wsi1212 <wsi1212@dgsw.hs.kr>
2025-02-15 19:41:42 +0900

    add clothes card view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.component.ClothesCard
+import com.example.myapplication.ui.model.Clothes
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +26,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Row {
+                        ClothesCard(
+                            modifier = Modifier.padding(innerPadding),
+                            item = Clothes(
+                                name = "T-shirt",
+                                price = 1000,
+                                image = "https://image.msscdn.net/thumbnails/images/goods_img/20240214/3866831/3866831_17095239369616_big.jpg?w=1200"
+                            )
+                        )
+                        Spacer(modifier = Modifier.width(20.dp))
+                        ClothesCard(
+                            modifier = Modifier.padding(innerPadding),
+                            item = Clothes(
+                                name = "T-shirt",
+                                price = 1000,
+                                image = "https://image.msscdn.net/thumbnails/images/goods_img/20240214/3866831/3866831_17095239369616_big.jpg?w=1200"
+                            )
+                        )
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/myapplication/ui/component/ClothesCard.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.compose.rememberAsyncImagePainter
+import com.example.myapplication.ui.model.Clothes
+
+@Composable
+fun ClothesCard(item: Clothes, modifier: Modifier = Modifier) {
+    AsyncImage(
+        modifier = modifier
+            .width(161.dp)
+            .height(206.dp),
+        model = item.image,
+        contentDescription = item.name,
+    )
+}
+
+@Preview
+@Composable
+fun ClothesCardPreview() {
+    ClothesCard(
+        Clothes(
+            name = "T-shirt",
+            price = 1000,
+            image = "https://image.msscdn.net/thumbnails/images/goods_img/20240214/3866831/3866831_17095239369616_big.jpg?w=1200"
+        )
+    )
+}
