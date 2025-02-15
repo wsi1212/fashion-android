46319d4dd86b930a79b05b995d3e7844cc8b4357
김도영 <binj1246@gmail.com>
2025-02-16 02:56:48 +0900

    add select picture

--- app/src/main/java/com/example/myapplication/ui/component/BottomNav.kt
+++ app/src/main/java/com/example/myapplication/ui/component/BottomNav.kt
@@ -1,29 +1,17 @@
 package com.example.myapplication.ui.component
 
+import android.net.Uri
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,7 +20,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -54,6 +41,16 @@ fun BottomNav(
     navController: NavController = NavController(LocalContext.current),
     modifier: Modifier = Modifier
 ) {
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri: Uri? ->
+            selectedImageUri = uri
+            Log.d("select picture", "picture uri : $uri")
+        }
+    )
+
     val views = listOf(
         View(
             icon = R.drawable.home_icon,
@@ -131,7 +128,6 @@ fun BottomNav(
             }
         }
 
-        // 중앙 버튼 배치 (입혀보기)
         Column(
             Modifier
                 .align(Alignment.BottomCenter)
@@ -142,7 +138,11 @@ fun BottomNav(
                     .size(80.dp, 80.dp)
                     .clip(CircleShape)
                     .background(Color(0xFF008BFF))
-                    .clickable { Log.d("fasd", "adf") }
+                    .clickable {
+                        photoPickerLauncher.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                        )
+                    }
             ) {
                 Image(
                     modifier = Modifier.align(Alignment.Center),
